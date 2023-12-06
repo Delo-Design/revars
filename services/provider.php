009bf58b17d0f5cf8f2920a207142b0559061820
dmitriitsymbal <cymbal@delo-design.ru>
2023-12-06 18:23:24 +0300

    добавил сервис провайдеры

---
+++ services/provider.php
@@ -0,0 +1,34 @@
+<?php \defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\System\Revars\Extension\Revars;
+
+return new class implements ServiceProviderInterface {
+
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function register(Container $container)
+	{
+		$container->set(PluginInterface::class,
+			function (Container $container) {
+				$plugin  = PluginHelper::getPlugin('system', 'revars');
+				$subject = $container->get(DispatcherInterface::class);
+
+				$plugin = new Revars($subject, (array) $plugin);
+				$plugin->setApplication(Factory::getApplication());
+
+				return $plugin;
+			}
+		);
+	}
+};
